284dc0b9d0a7b7fd81127d50039353132a51cabd
ĐÌNH AN <115713179+dinhan195@users.noreply.github.com>
2023-04-14 20:54:02 +0700

    bt1

--- day12-react/Practice/react-demo/src/App.jsx
+++ day12-react/Practice/react-demo/src/App.jsx
@@ -1,7 +1,7 @@
 /** @format */
 import './App.css';
 import MyApp from './components/lesson-12-func-component/Exercise/Exercise-1';
-import SetTime from './components/lesson-12-func-component/Exercise/Exercise-2';
+// import SetTime from './components/lesson-12-func-component/Exercise/Exercise-2';
 // import { UseMemo } from './components/lesson-12-func-component/Practice/Practice-3';
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       {/* <Hello/> */}
       {/* <UseMemo/> */}
       <MyApp />
-      <SetTime/>
+//       <SetTime/>
     </div>
   );
 }
