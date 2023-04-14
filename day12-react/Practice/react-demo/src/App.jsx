ebadf5d37e8ca88b7a3c093615a99e23164beea3
ĐÌNH AN <115713179+dinhan195@users.noreply.github.com>
2023-04-14 21:04:57 +0700

    ok

--- day12-react/Practice/react-demo/src/App.jsx
+++ day12-react/Practice/react-demo/src/App.jsx
@@ -1,20 +1,12 @@
 /** @format */
 import './App.css';
 import MyApp from './components/lesson-12-func-component/Exercise/Exercise-1';
-// import SetTime from './components/lesson-12-func-component/Exercise/Exercise-2';
-// import { UseMemo } from './components/lesson-12-func-component/Practice/Practice-3';
+
 
 function App() {
   return (
     <div className="App">
-      {/* <TryUseState/> */}
-      {/* <Practice4/> */}
-      {/* <MyComponent/> */}
-      {/* <HelloComponent/> */}
-      {/* <Hello/> */}
-      {/* <UseMemo/> */}
       <MyApp />
-//       <SetTime/>
     </div>
   );
 }
