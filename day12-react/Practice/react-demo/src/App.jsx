3f5563d78c381f4fb543d9838b6d20356e35a27c
ĐÌNH AN <115713179+dinhan195@users.noreply.github.com>
2023-04-19 15:47:11 +0700

    dfd

--- day12-react/Practice/react-demo/src/App.jsx
+++ day12-react/Practice/react-demo/src/App.jsx
@@ -1,22 +1,10 @@
 /** @format */
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
 import router from './router';
 
 function App() {
   return <RouterProvider router={router}></RouterProvider>;
-=======
-import MyApp from './components/lesson-12-func-component/Exercise/Exercise-1';
-
-
-function App() {
-  return (
-    <div className="App">
-      <MyApp />
-    </div>
-  );
->>>>>>> ebadf5d37e8ca88b7a3c093615a99e23164beea3
 }
 
 export default App;
--- day12-react/Practice/react-demo/src/components/lesson-12-func-component/Exercise/Exercise-2/index.jsx
+++ day12-react/Practice/react-demo/src/components/lesson-12-func-component/Exercise/Exercise-2/index.jsx
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from 'react';
+/** @format */
 
+import React, { useEffect, useState } from 'react';
 
 function SetTime() {
   const [time, setTime] = useState(1);
-  const [message, setMessage] = useState()
+  const [message, setMessage] = useState();
 
   useEffect(() => {
     setTimeout(() => {
       setTime(time + 1);
     }, 1000);
     if (time >= 5) {
-      setMessage(new Date().toLocaleTimeString())
-      setTime(0)
+      setMessage(new Date().toLocaleTimeString());
+      setTime(0);
     }
   }, [time]);
- 
 
+  //jdfd
   return (
     <div>
       <h2>Bài tập 2</h2>
@@ -23,7 +24,6 @@ function SetTime() {
       <p>Current time: {message}</p>
     </div>
   );
-
 }
 
 export default SetTime;
